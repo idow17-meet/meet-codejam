87658d0d9f5e8c754ad9402fbd36365cbeae7073
idow17-meet <idow17@meet.mit.edu>
2019-05-18 18:43:45 +0300

    Added background image

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -23,6 +23,9 @@ export default {
 <style lang="scss" scoped>
 @import '~@/assets/styles/font-registration.scss';
 
+$bg-color: #0D435B;
+$bg-color-dark: #092B3A;
+
 #app {
   font-family: 'Proxima Nova';
   font-weight: 600;
@@ -30,5 +33,12 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
+  background: url('~@/assets/img/bg-pattern.png'), $bg-color;
+  background: url('~@/assets/img/bg-pattern.png'), -webkit-linear-gradient(to left, $bg-color, $bg-color-dark);
+  background: url('~@/assets/img/bg-pattern.png'), linear-gradient(to left, $bg-color, $bg-color-dark);
+
+  min-height: 100vh;
+  padding-bottom: 20px;
 }
 </style>
