e657b723cb0797d2fa412b06cdd2d53b0e898765
Ido Wiernik <idow17@meet.mit.edu>
2019-07-15 22:29:36 +0300

    Scores API removes hint from scores where hint wasn't used

--- backend/api/scores.py
+++ backend/api/scores.py
@@ -4,12 +4,30 @@ from starlette.status import HTTP_404_NOT_FOUND
 from typing import List
 import logging
 
-from backend.database.model import DBGroup, OutScore
-from .database import get_db
+from backend.database.model import DBGroup, OutScore, DBScore
+from .database import get_db, CodejamDB
 from .auth import get_current_user
 
 
 scores = APIRouter()
+
+
+def create_out_score(db_session: CodejamDB, score: DBScore) -> OutScore:
+    """
+    Converts a Database score to an OutScore (binds the problem instance and removes hint if necessary)
+
+    :param db_session: The current database session to query with
+    :param score: The score as queried from the database
+    :return: A new OutScore instance with a binded problem
+    """
+    problem = db_session.get_problem(score.problem_id)
+    if not problem:
+        raise ValueError(f"Problem not found for score: {score}")
+
+    out_score = OutScore(**score.dict(), problem=problem)
+    if not score.hint_used:
+        out_score.problem.hint = None
+    return out_score
 
 
 @scores.get('/', response_model=List[OutScore], response_model_exclude={'group_id'})
@@ -25,12 +43,11 @@ def get_group_scores(group_name: str, current_user: DBGroup = Depends(get_curren
         raise HTTPException(status_code=HTTP_404_NOT_FOUND,
                             detail="No scores found for the given group name")
     output_scores = []
-    for problem in db_session.get_all_problems():
+    for score in group_scores:
         try:
-            score = [score for score in group_scores if score.problem_id == problem.problem_id][0]
-            output_scores.append(OutScore(**score.dict(), problem=problem))
-        except IndexError:
-            logging.error(f"Score not found for problem id: {problem.problem_id}")
+            output_scores.append(create_out_score(db_session, score))
+        except ValueError as e:
+            logging.error(str(e))
 
     output_scores.sort(key=lambda score: score.problem.difficulty)
     return output_scores
@@ -44,9 +61,13 @@ def get_group_score(group_name: str, problem_name: str, current_user: DBGroup = 
     if not score:
         raise HTTPException(status_code=HTTP_404_NOT_FOUND,
                             detail="Score not found")
-    problem = db_session.get_problem(problem_name)
-    new_score = OutScore(**score.dict(), problem=problem)
-    return new_score
+
+    try:
+        return create_out_score(db_session, score)
+    except ValueError as e:
+        logging.error(str(e))
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND,
+                            detail="Problem not found for score instance")
 
 
 @scores.post('/{problem_name}')
