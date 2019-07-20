e9b3bde038be8b44452eb5ac5e2daaacf23b9cd6
Ido Wiernik <idow17@meet.mit.edu>
2019-07-20 13:06:40 +0300

    Added groups API

---
+++ backend/api/groups.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, Depends
+from typing import List, Dict
+
+from .database import get_db
+from .auth import get_current_user
+from backend.database.model import DBGroup, OutGroup
+
+
+groups = APIRouter()
+
+
+@groups.get('/', response_model=Dict[str, List[OutGroup]])
+def get_all_groups(current_user: DBGroup = Depends(get_current_user)):
+    db_session = get_db()
+    return {'groups': db_session.get_all_groups()}
+
+
+@groups.get('/{group_name}', response_model=OutGroup)
+def get_group(group_name: str, current_user: DBGroup = Depends(get_current_user)):
+    db_session = get_db()
+    return db_session.get_group(group_name)
