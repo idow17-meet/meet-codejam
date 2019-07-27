93fbc33717d67bb60b02551c7d1a47cc9f678a1a
george-sarji <george.s@nazdaq-it.com>
2019-07-27 21:14:42 +0300

    Added groups filter according to current user admin status

--- backend/api/groups.py
+++ backend/api/groups.py
@@ -12,6 +12,7 @@ groups = APIRouter()
 @groups.get('/', response_model=Dict[str, List[OutGroup]])
 def get_all_groups(current_user: DBGroup = Depends(get_current_user)):
     db_session = get_db()
+    all_groups = db_session.get_all_groups() if current_user.admin else filter(lambda group: group.admin, db_session.get_all_groups())
     return {'groups': db_session.get_all_groups()}
 
 
